f2f8dcc84df701be27ed9ed86b66ad4e5a94e6ac
ghdwn941108 <ghdwn941108@gmail.com>
2026-08-20 14:06:48 +0900

    260820 공지사항 조회 프론트
    1. app 에 route 연동
    2. main :  brouserrouter 임포트
    3. noticeapi : selectnoticeApi 작성
    4. itemcomponent 작성
    5. listComponent 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import NoticeListComponent from './notice/component/NoticeListComponent'
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+
+      <Routes>
+        <Route path="/notice/list" element={<NoticeListComponent/>}/>
+
+      </Routes>
+    </div>
   )
 }
 
