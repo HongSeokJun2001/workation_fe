71ac60631582ad36aef719cbd762d495fc77e10c
min10288291 <mjkim20030117@gmail.com>
2026-08-21 17:18:14 +0900

    20260821_워케이션신청리스트화면&워케이션신청화면
    워케이션신청리스트 백엔드 연동
    워케이션 신청 화면 구현중

--- src/workation/components/WorkationItemComponent.jsx
+++ src/workation/components/WorkationItemComponent.jsx
@@ -1,21 +1,22 @@
 import { useNavigate } from "react-router-dom";
 
-function WorkationItemComponent(props) {
-    
-    let navigate = userNavigate();
-
-    const item = props.item;
+function WorkationItemComponent({ item }) {
+    let navigate = useNavigate();
 
     return (
-        <tr onClick={ () => {navigate('/workation/detail/${ item.workavationId }');}}>
-            <td>{ item. }</td>
-            <td>{ item. }</td>
-            <td>{ item. }</td>
-            <td>{ item. }</td>
-            <td>{ item. }</td>
+        <tr 
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+                navigate(`/workation/detail/${item.workationId}`);
+            }}
+        >
+            <td> {item.crew.crewName }</td>
+            <td>{item.reservationDate.startDate} ~ {item.reservationDate.endDate}</td>
+            <td>{ item.region }</td>
+            <td>{ item.status }</td>
+            <td>{ item.createDate.substring(0, 10) }</td>
         </tr>
     );
-
 }
 
 export default WorkationItemComponent;
