236cab743085e9adc0193be53224dcd91349740a
ghdwn941108 <ghdwn941108@gmail.com>
2026-09-08 02:40:13 +0900

    260908_crew프론트 완성, 댓글기능추가
    1. 프론트 완성
    2.ui 완성
    3. 댓글 프론트 완성

--- src/crew/api/CrewApi.js
+++ src/crew/api/CrewApi.js
@@ -134,6 +134,22 @@ const selectMyCrewListApi = () => {
     return response;
 }
 
+const selectMyCreatedCrewListApi = () => axios({
+    url: `${BASE_URL}/mycreated`,
+    method: "get",
+    headers: {
+        Authorization: getAuthorization()
+    }
+});
+
+const selectCrewPageApi = crewId => axios({
+    url: `${BASE_URL}/position/${crewId}`,
+    method: "get",
+    headers: {
+        Authorization: getAuthorization()
+    }
+});
+
 
 //크루 신청하기 api
 const joinCrewApi = crewId =>{
@@ -194,5 +210,7 @@ export { selectCrewListApi, selectCrewApi, searchCrewListApi, insertCrewApi, upd
     leaveCrewApi,
     selectCrewMemberNamesApi,
     selectMyCrewListApi,
+    selectMyCreatedCrewListApi,
+    selectCrewPageApi,
     selectCrewLeaderListApi
 };
